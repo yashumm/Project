fa4e0fc1bc6eb2e5ef9626ea1f35232715870259
yashumm <yashaswinmm@gmail.com>
2025-09-03 21:27:29 +0530

    Add files via upload

---
+++ Arr4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Arraysum{
+    public static void main(String[] args) {
+        scanner scanner = new scanner(System.in);
+        System.out.print("Enter the size of the array;");
+        int size = Scanner.next int[size];
+        System.out.println("Enetr"+size+"numbers");
+        for(iint i=0;i<size;i++)numbers[i]=scanner.nextxint();
+        System.out.println("enter array elemtns;")
+    }
+}
