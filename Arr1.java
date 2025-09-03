a2607ebf419e6f046e07e7c942dffff1b41feaa9
yashumm <yashaswinmm@gmail.com>
2025-09-03 21:25:39 +0530

    Add files via upload

---
+++ Arr1.java
@@ -0,0 +1,16 @@
+public class Arr1{
+    public static void main(String[] args) {
+        
+    }
+        int n[]= new int[6];
+        n[0]=5;
+        n[1]=6;
+        n[2]=7;
+        n[3]=8;
+        n[4]=9;
+        n[5]=10;
+        for(int i=0;i<5;i++){
+            System.out.println(n[i]);
+        }
+        }
+}
